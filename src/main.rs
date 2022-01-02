67717201617847031a34215c9379774533f8f11d
Wildhoney <adam.timberlake@gmail.com>
2022-01-02 23:21:06 +0000

    Added 'get_envs' function

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,28 @@ use std::fs::OpenOptions;
 use std::io::Write;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let event = &*args[1].to_lowercase();
-    let item = &*args[2];
+    let (event, value) = get_envs();
 
-    match event {
-        "add" => match with_file().write_all(format!("{}\n", item).as_bytes()) {
-            Err(_) => panic!("couldn't write file."),
-            Ok(_) => println!("successfully wrote to file."),
+    match event.as_str() {
+        "add" => match with_file().write_all(format!("{}\n", value).as_bytes()) {
+            Err(_) => panic!("Failed to add todo."),
+            Ok(_) => println!("Added todo."),
         },
-        "remove" => println!("Remove!"),
+        "remove" => println!("Removed todo."),
         _ => println!("Not sure really?"),
     }
+}
+
+fn get_envs() -> (String, String) {
+    let args: Vec<String> = env::args().collect();
+    let event = args[1].to_lowercase();
+    let value = if args.len() == 2 {
+        String::from("")
+    } else {
+        args[2].to_string()
+    };
+
+    return (event, value);
 }
 
 fn with_file() -> std::fs::File {
